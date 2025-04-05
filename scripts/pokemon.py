64168ada2cab42fcb77ee526ada355f2d572b57f
Aravind Pramod <aravindpramod@outlook.com>
2025-04-05 13:57:50 +0530

    pokemon dataset - 5 Apr 2025

--- scripts/pokemon.py
+++ scripts/pokemon.py
@@ -1,7 +1,18 @@
+import pandas as pd
 import polars as pl
 
-# Option 2: Read directly from GitHub and assign to an object
+# Read directly from GitHub and assign to an object
 
-pokemon_df = pl.read_csv(
-    "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-04-01/pokemon_df.csv"
+pokemon_df = pl.from_pandas(
+    pd.read_csv(
+        "https://raw.githubusercontent.com/rfordatascience/tidytuesdayu/main/data/2025/2025-04-01/pokemon_df.csv"
+    )
 )
+
+# Display the first few rows of the DataFrame
+pokemon_df.head()
+
+pokemon_df.columns
+
+pokemon_df.shape
+pokemon_df.dtypes
